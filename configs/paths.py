954741b4318a0023454af963ea9e6b03a30db9ae
Maverick Wong <mvrck.wong@gmail.com>
2025-10-19 21:48:39 +0800

    Update the base configuration

---
+++ configs/paths.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+# Project directory
+PROJECT_ROOT: Path = Path(__file__).resolve().parent.parent.parent
+
+# Directories
+LOGS_DIR: Path = PROJECT_ROOT / "logs"
+DATA_DIR: Path = PROJECT_ROOT / "data"
+OUTPUT_DIR: Path = PROJECT_ROOT / "output"
+IMAGE_DIR: Path = PROJECT_ROOT / "images"
+
+ENV_FILE: Path = PROJECT_ROOT / ".env"
+
+
+if __name__ == "__main__":
+      pass
